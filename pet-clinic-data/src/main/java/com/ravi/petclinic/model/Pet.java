6881c8aa28de671edd5b66b8504ef6d578d0982b
Ravi.Cherukuri <vcheruk2@ncsu.edu>
2020-03-09 02:27:49 -0700

    Added Long Id for Model Objects. Closes #9

--- pet-clinic-data/src/main/java/com/ravi/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/ravi/petclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.ravi.petclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
 
     private PetType type;
     private Owner owner;
--- pet-clinic-data/src/main/java/com/ravi/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/ravi/petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.ravi.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String type;
 
