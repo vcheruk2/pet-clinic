41e7e4d5da0be300dd531ef77f3d8fdd412ea201
Ravi.Cherukuri <vcheruk2@ncsu.edu>
2020-03-10 23:00:08 -0700

    Created Owner Index Page & Controller. Closes #12

---
+++ pet-clinic-web/src/main/java/com/ravi/petclinic/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package com.ravi.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"","/", "/index.html", "/index"})
+    public String listOwners(){
+
+        return "owners/owners";
+    }
+}
