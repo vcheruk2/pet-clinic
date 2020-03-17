bc7c953bb2d63a0c6bd257429c7f53a4f4cbea46
Ravi.Cherukuri <vcheruk2@ncsu.edu>
2020-03-16 23:41:55 -0700

    Listed all vets on the index page. Closes #14

--- pet-clinic-web/src/main/java/com/ravi/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/ravi/petclinic/controllers/VetController.java
@@ -1,13 +1,22 @@
 package com.ravi.petclinic.controllers;
 
+import com.ravi.petclinic.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String listVets(){
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/vet","/vets","/vets/index","/vets/index.html"})
+    public String listVets(Model model){
+        model.addAttribute("vets", this.vetService.findAll());
         return "vets/vets";
     }
 }
