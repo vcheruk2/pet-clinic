1a8a219dbec6d696450fe49720acc38d9c557863
Ravi.Cherukuri <vcheruk2@ncsu.edu>
2020-03-30 00:19:23 -0700

    Created Spring Data JPA Repositories. Closes #38

---
+++ pet-clinic-data/src/main/java/com/ravi/petclinic/repository/SpecialtyRepository.java
@@ -0,0 +1,9 @@
+package com.ravi.petclinic.repository;
+
+import com.ravi.petclinic.model.Pet;
+import org.springframework.data.repository.CrudRepository;
+
+/* Created by: Venkata Ravichandra Cherukuri
+   Created on: 3/30/2020 */
+public interface SpecialtyRepository extends CrudRepository<Pet, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/ravi/petclinic/repository/VetRepository.java
@@ -0,0 +1,9 @@
+package com.ravi.petclinic.repository;
+
+import com.ravi.petclinic.model.Vet;
+import org.springframework.data.repository.CrudRepository;
+
+/* Created by: Venkata Ravichandra Cherukuri
+   Created on: 3/30/2020 */
+public interface VetRepository extends CrudRepository<Vet, Long> {
+}
