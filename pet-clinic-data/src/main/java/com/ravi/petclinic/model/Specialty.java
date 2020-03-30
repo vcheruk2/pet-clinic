5e5ecc00a0522b1b8fd526226f0f7f1406cfc4d9
Ravi.Cherukuri <vcheruk2@ncsu.edu>
2020-03-29 20:40:25 -0700

    Converted Vets & Specialty to JPA entity. Closes #36

--- pet-clinic-data/src/main/java/com/ravi/petclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/com/ravi/petclinic/model/Specialty.java
@@ -1,7 +1,14 @@
 package com.ravi.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Specialty extends BaseEntity{
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/com/ravi/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/ravi/petclinic/model/Vet.java
@@ -1,9 +1,16 @@
 package com.ravi.petclinic.model;
 
+import javax.persistence.*;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities",
+        joinColumns = @JoinColumn(name = "vet_id"),
+        inverseJoinColumns = @JoinColumn(name = "speicality_id"))
     private Set<Specialty> specialties;
 
     public Set<Specialty> getSpecialties() {
