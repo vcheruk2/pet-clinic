4707ad680879fe4565ba8e2e9b061e05b2021dc2
Ravi.Cherukuri <vcheruk2@ncsu.edu>
2020-04-26 17:29:20 -0700

    Implemented Vets JSON endpoint. Closes #65

--- pet-clinic-web/src/main/java/com/ravi/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/ravi/petclinic/controllers/VetController.java
@@ -1,9 +1,13 @@
 package com.ravi.petclinic.controllers;
 
+import com.ravi.petclinic.model.Vet;
 import com.ravi.petclinic.service.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -18,5 +22,11 @@ public class VetController {
     public String listVets(Model model){
         model.addAttribute("vets", this.vetService.findAll());
         return "vets/vets";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+
+        return vetService.findAll();
     }
 }
